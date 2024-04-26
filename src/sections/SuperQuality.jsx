214ba74695f6accdf6919f3505c29fd019618c14
Maxi-echaide <maxi.echaide@gmail.com>
2024-04-26 04:39:30 -0300

    super quality section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -25,7 +25,7 @@ const SuperQuality = () => {
         </div>
       </div>
       <div className='felx-1 flex justify-center items-center'>
-        <img src={shoe8} alt="shoe8" width={570} height={522} />
+        <img src={shoe8} alt="shoe8" width={570} height={522} className='object-contain'/>
       </div>
     </section>
   );
